d92f466a20250df08bcb7204cc7159af6afaaeb4
Nicolas Gardien <Nicolasgardien11@gmail.com>
2022-08-28 23:38:02 +0200

    formulaire teamHome sur scoreboard

--- src/pages/Scoreboard.js
+++ src/pages/Scoreboard.js
@@ -2,35 +2,43 @@ import React from "react";
 import Navigation from "../components/Navigation";
 
 const Scoreboard = () => {
+  let teamH = "";
+
+  const teamHome = (e) => {
+    teamH = e.target.value;
+  };
+
+  const btn = (e) => {
+    e.preventDefault();
+
+    if (teamH.length < 3) {
+      document.getElementById('error').innerHTML="Veuillez entrez 3 caractères minimum";
+      
+    } else {
+      document.getElementById('error').style.display= 'none'
+      console.log(teamH)
+    }
+  };
+
   return (
     <div className="scoreboard">
       <Navigation />
-      <div className="teamName">
-        <form className="teamHome">
-          <input
-            class="form-control"
-            type="text"
-            placeholder="Nom de l'équipe à domicile"
-            aria-label="Nom de l'équipe à domicile"
-          />
-          <label for="exampleColorInput" class="form-colorHome">
-            Couleur du maillot
-          </label>
-        </form>
-        <form className="teamVisitor">
-          <input
-            class="form-control"
-            type="text"
-            placeholder="Nom de l'équipe visiteuse"
-            aria-label="Nom de l'équipe visiteuse"
-          />
-        </form>
-      </div>
-      <div class="col-4 btnsubmit">
-        <button class="btn btn-primary" type="submit">
-          Confirmer les équipes
-        </button>
-      </div>
+      <form className="teamHome">
+        <label id="teamHomeLabel" htmlFor="teamHome"> Nom de l'équipe à domicile</label>
+        <input
+          onChange={teamHome}
+          className="form-control-home teamHome-container"
+          type="text"
+          autoComplete="off"
+          id="teamHome"
+          placeholder="Entrez le nom de l'équipe ici"
+          aria-label="Nom de l'équipe à domicile"
+          name="team"
+        />
+        <span id="error"></span>
+
+        <input onClick={btn} type="submit" value="Valider" className="btn" />
+      </form>
     </div>
   );
 };
