49f8021e9f3a9df1f386e7d9537f0a74657ed950
Nicolas Gardien <Nicolasgardien11@gmail.com>
2022-08-31 13:38:09 +0200

    component teamchoice logique JS FINI

--- src/components/Teamchoice.js
+++ src/components/Teamchoice.js
@@ -1,116 +1,118 @@
-import React from 'react';
+import React from "react";
 
 const Teamchoice = () => {
-    let teamH = "";
-    let teamV = "";
-    let colorH = "";
-    let colorV = "";
-  
-    const teamHome = (e) => {
-      teamH = e.target.value;
-    };
-  
-    const teamVisitor = (e) => {
-      teamV = e.target.value;
-    };
-  
-    const btn = (e) => {
-      e.preventDefault();
-  
-      if (teamH.length < 3) {
-        document.getElementById("errorH").innerHTML =
-          "Veuillez entrez 3 caractères minimum";
-      }
-      if (teamV.length < 3) {
-        document.getElementById("errorV").innerHTML =
-          "Veuillez entrez 3 caractères minimum";
-      } else {
-        document.getElementById("errorV").style.display = "none";
-        document.getElementById("errorH").style.display = "none";
-        console.log(teamH);
-        console.log(teamV);
-        console.log(colorH);
-        console.log(colorV);
-      }
-    };
-  
-    const colorHome = (e) => {
-      colorH = e.target.value;
-  
-      document.getElementById("colorInputHome").value = `${colorH}`;
-    };
-  
-    const colorVisitor = (e) => {
-      colorV = e.target.value;
-  
-      document.getElementById("colorInputVisitor").value = `${colorV}`;
-    };
-  
-    return (
-      <div className="teamchoice">
-        <div className="container-fluid first-container">
-          <div className="team-container">
-            <form className="teamHome">
-              <label id="teamHomeLabel" htmlFor="teamHome">
-                Nom de l'équipe à domicile
-              </label>
-              <input
-                onChange={teamHome}
-                className="form-control-home teamHome-container"
-                type="text"
-                autoComplete="off"
-                id="teamHome"
-                placeholder="Entrez le nom de l'équipe ici"
-                aria-label="Nom de l'équipe à domicile"
-                name="team"
-              />
-              <label for="colorInputHome" class="form-label">
-                Couleur du maillot
-              </label>
-              <input
-                onChange={colorHome}
-                type="color"
-                class="form-control form-control-color"
-                id="colorInputHome"
-                title="Choisissez votre couleur"
-              />
-              <span id="errorH"></span>
-            </form>
-          </div>
-  
-          <div className="team-container">
-            <form action="">
-              <label id="teamVisitorLabel" htmlFor="teamVisitor">
-                Nom de l'équipe à l'exterieur
-              </label>
-              <input
-                onChange={teamVisitor}
-                className="form-control-visitor teamVisitor-container"
-                type="text"
-                autoComplete="off"
-                id="teamVisitor"
-                placeholder="Entrez le nom de l'équipe ici"
-                aria-label="Nom de l'équipe à l'exterieur"
-              />
-              <label for="colorInputVisitor" class="form-label">
-                Couleur du maillot
-              </label>
-              <input
-                onChange={colorVisitor}
-                type="color"
-                class="form-control form-control-color"
-                id="colorInputVisitor"
-                title="Choisissez votre couleur"
-              />
-              <span id="errorV"></span>
-            </form>
-          </div>
+  let teamH = "";
+  let teamV = "";
+  let colorH = "";
+  let colorV = "";
+
+  const teamHome = (e) => {
+    teamH = e.target.value;
+
+    if (teamH.length < 3) {
+      document.getElementById("errorH").style.visibility = "visible";
+      document.getElementById("errorH").innerHTML = "3 caractères obligatoire";
+    } else {
+      document.getElementById("errorH").style.visibility = "hidden";
+    }
+  };
+
+  const teamVisitor = (e) => {
+    teamV = e.target.value;
+
+    if (teamV.length < 3) {
+      document.getElementById("errorV").style.visibility = "visible";
+      document.getElementById("errorV").innerHTML = "3 caractères obligatoire";
+    } else {
+      document.getElementById("errorV").style.visibility = "hidden";
+    }
+  };
+
+  const colorHome = (e) => {
+    console.log(colorH);
+    colorH = e.target.value;
+  };
+
+  const colorVisitor = (e) => {
+    colorV = e.target.value;
+  };
+
+  const btn = (e) => {
+    e.preventDefault();
+
+    if ((colorV.length && colorH.length && teamH.length && teamV.length)< 3) {
+      document.getElementById("error").innerHTML =
+        "Veuillez renseigner tous les champs";
+    } else {
+      document.getElementById("error").style.visibility =
+        "hidden";
+    }
+  };
+  return (
+    <div className="teamchoice">
+      <div className="container-fluid first-container">
+        <div className="team-container">
+          <form className="teamHome">
+            <label id="teamHomeLabel" htmlFor="teamHome">
+              Nom de l'équipe à domicile
+            </label>
+            <input
+              onChange={teamHome}
+              className="form-control-home teamHome-container"
+              type="text"
+              autoComplete="off"
+              id="teamHome"
+              placeholder="Entrez le nom de l'équipe ici"
+              aria-label="Nom de l'équipe à domicile"
+            />
+            <span id="errorH"></span>
+            <label htmlFor="colorInputHome" class="form-label">
+              Couleur du maillot
+            </label>
+            <input
+              onChange={colorHome}
+              type="color"
+              className="form-control form-control-color"
+              id="colorInputHome"
+              title="Choisissez votre couleur"
+            />
+          </form>
         </div>
-        <form action="">
-          <input onClick={btn} type="submit" value="Valider" className="btn" />
-        </form>
+
+        <div className="team-container">
+          <form action="">
+            <label id="teamVisitorLabel" htmlFor="teamVisitor">
+              Nom de l'équipe à l'exterieur
+            </label>
+            <input
+              onChange={teamVisitor}
+              className="form-control-visitor teamVisitor-container"
+              type="text"
+              autoComplete="off"
+              id="teamVisitor"
+              placeholder="Entrez le nom de l'équipe ici"
+              aria-label="Nom de l'équipe à l'exterieur"
+            />
+            <span id="errorV"></span>
+            <label htmlFor="colorInputVisitor" class="form-label">
+              Couleur du maillot
+            </label>
+            <input
+              onChange={colorVisitor}
+              type="color"
+              className="form-control form-control-color"
+              id="colorInputVisitor"
+              title="Choisissez votre couleur"
+            />
+          </form>
+        </div>
       </div>
-    );
+      <form action="">
+        <input onClick={btn} type="submit" value="Valider" className="btn" />
+      </form>
+      <span id="error"></span>
+    </div>
+  );
 };
 
 export default Teamchoice;
